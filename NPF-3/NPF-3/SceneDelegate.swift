ab92d3e9ed38039167055725b2502bb125fafbad
Ellie Parobek <ellie.parobek@gmail.com>
2020-03-24 15:09:39 -0400

    NP-3 started

---
+++ NPF-3/NPF-3/SceneDelegate.swift
@@ -0,0 +1,87 @@
+//
+//  SceneDelegate.swift
+//  NPF-3
+//
+//  Created by Ellie on 3/24/20.
+//  Copyright © 2020 Ellie. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    var parks: [Park] = []
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        // Load data
+        if let path = Bundle.main.path(forResource: "data", ofType: "plist") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path))
+                let tempDict = try PropertyListSerialization.propertyList(from: data, format: nil) as! [String:Any]
+                let tempArray = tempDict["parks"] as! Array<[String:Any]>
+                
+                for dict in tempArray {
+                    let parkName = dict["parkName"]! as! String
+                    let parkLocation = dict["parkLocation"]! as! String
+                    let latitude = Double(dict["latitude"]! as! String)!
+                    let longitude = Double(dict["longitude"]! as! String)!
+                    let location = CLLocation(latitude: latitude, longitude: longitude)
+                    let imageLink = dict["imageLink"]! as! String
+                    let link = dict["link"]! as! String
+                    let dateFormed = dict["dateFormed"]! as! String
+                    let description = dict["description"]! as! String
+                    let area = dict["area"]! as! String
+                    
+                    let p = Park(parkName: parkName, parkLocation: parkLocation, dateFormed: dateFormed, area: area, link: link, location: location, imageLink: imageLink, parkDescription: description)
+                    parks.append(p)
+                }
+                
+                // Check to see if the parks were created correctly
+                for p in parks {
+                    print("Park: \(p)")
+                }
+                
+            } catch {
+                print(error)
+            }
+        }
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+
+
+}
+
---
+++ NPF-3/NPF-3/ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  NPF-3
+//
+//  Created by Ellie on 3/24/20.
+//  Copyright © 2020 Ellie. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var toolBar: UIToolbar!
+    
+    var parks: [Park] = []
+    var locationManager: CLLocationManager
+    
+    required init?(coder: NSCoder) {
+        locationManager = CLLocationManager()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get all of the parks from the SceneDelegate
+        let scene = UIApplication.shared.connectedScenes.first
+        if let delegate : SceneDelegate = (scene?.delegate as? SceneDelegate) {
+            parks = delegate.parks
+        }
+        
+        print("Number of parks: \(parks.count)")
+        for park: Park in parks {
+            mapView.addAnnotation(park)
+        }
+        mapView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        let annotation = view.annotation
+        print("The title of the annotation is: \(String(describing:annotation?.title))")
+    }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var view: MKPinAnnotationView
+        let identifier = "Pin"
+        if annotation is MKUserLocation {
+            //if annotation is not an MKPointAnnotation (eg. MKUserLocation),
+            //return nil so map draws default view for it (eg. blue dot)...
+            return nil
+        }
+
+        if annotation !== mapView.userLocation {
+            //look for an existing view to reuse
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView {
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            }
+            else {
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                // view.pinColor = MKPinAnnotationColor.Purple
+                view.pinTintColor = MKPinAnnotationView.purplePinColor()
+                view.animatesDrop = true
+                view.canShowCallout = true
+                //view.calloutOffset = CGPoint(x: -5, y: 5)
+                let leftButton = UIButton(type: .infoLight)
+                let rightButton = UIButton(type: .detailDisclosure)
+                leftButton.tag = 0
+                rightButton.tag = 1
+                view.leftCalloutAccessoryView = leftButton
+                view.rightCalloutAccessoryView = rightButton
+            }
+            return view
+        }
+        return nil
+    }
+
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+
+        let parkAnnotation = view.annotation as! Park
+
+        switch control.tag {
+            case 0: //left button
+                if let url = URL(string: parkAnnotation.getLink()) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            case 1: //right button
+                let coordinate = locationManager.location?.coordinate //make sure location manager has updated before trying to use
+                let url = String(format: "http://maps.apple.com/maps?saddr=%f,%f&daddr=%f,%f", coordinate?.latitude ?? 0,coordinate?.longitude ?? 0,(parkAnnotation.getLocation()?.coordinate.latitude)!,parkAnnotation.getLocation()?.coordinate.longitude ?? 0)
+                UIApplication.shared.open(URL(string: url)!, options: [:], completionHandler: nil)
+            default:
+                break
+        }
+    }
+}
+
